9617d6e8da6cfdb29aea3767235550b9666c252c
Joshua Ryan-Sprowson <dev.joshuars@gmail.com>
2025-07-02 14:19:26 +0800

    Improved the page responsiveness on different screen sizes

--- ReactApp/src/components/pages/ProjectSettings/ProjectSettings.tsx
+++ ReactApp/src/components/pages/ProjectSettings/ProjectSettings.tsx
@@ -7,28 +7,36 @@ import ProjectSettingsAdminstrative from '../../ProjectSettingsAdminstrative';
 const ProjectSettings: FC = () => {
   return (
     <>
-      <IconButton
-        aria-label="back"
-        color="primary"
-        sx={{
-          p: {
-            xs: 1,
-            md: 2,
-            lg: 3,
-          },
-        }}
-        component={Link}
-        to=".."
-        relative="path"
-      >
-        <ArrowBackIcon />
-      </IconButton>
-      <Typography variant="h1">Project Settings</Typography>
-      <Grid container spacing={2}>
-        <Grid size={{ xs: 6, md: 8 }}>
+      <Grid container sx={{ m: 2 }}>
+        <Grid size={{ xs: 12, md: 'auto' }}>
+          <IconButton
+            aria-label="back"
+            color="primary"
+            sx={{
+              p: {
+                xs: 1,
+                md: 2,
+                lg: 3,
+              },
+            }}
+            component={Link}
+            to=".."
+            relative="path"
+          >
+            <ArrowBackIcon />
+          </IconButton>
+        </Grid>
+        <Grid size={{ xs: 12, md: 'grow' }}>
+          <Typography variant="h1" textAlign="center">
+            Settings
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2} sx={{ m: 2 }}>
+        <Grid size={{ xs: 12, md: 6, lg: 'grow' }}>
           <Typography variant="h3">General</Typography>
         </Grid>
-        <Grid size={{ xs: 6, md: 4 }}>
+        <Grid size={{ xs: 12, md: 6, lg: 4 }}>
           <ProjectSettingsAdminstrative />
         </Grid>
       </Grid>
