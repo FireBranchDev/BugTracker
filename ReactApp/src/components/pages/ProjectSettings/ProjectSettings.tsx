106d361ddb27f72b3c21b17d725dccb9e2f7f0f9
Joshua Ryan-Sprowson <dev.joshuars@gmail.com>
2025-07-02 13:24:43 +0800

    Added a back arrow icon button

--- ReactApp/src/components/pages/ProjectSettings/ProjectSettings.tsx
+++ ReactApp/src/components/pages/ProjectSettings/ProjectSettings.tsx
@@ -1,10 +1,28 @@
-import { Grid, Typography } from '@mui/material';
-import type { FC } from 'react';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Grid, IconButton, Typography } from '@mui/material';
+import { type FC } from 'react';
+import { Link } from 'react-router-dom';
 import ProjectSettingsAdminstrative from '../../ProjectSettingsAdminstrative';
 
 const ProjectSettings: FC = () => {
   return (
     <>
+      <IconButton
+        aria-label="back"
+        color="primary"
+        sx={{
+          p: {
+            xs: 1,
+            md: 2,
+            lg: 3,
+          },
+        }}
+        component={Link}
+        to=".."
+        relative="path"
+      >
+        <ArrowBackIcon />
+      </IconButton>
       <Typography variant="h1">Project Settings</Typography>
       <Grid container spacing={2}>
         <Grid size={{ xs: 6, md: 8 }}>
