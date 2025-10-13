2567794594f236dc77d7dea4a12713da35be20e0
Joshua Ryan-Sprowson <dev.joshuars@gmail.com>
2025-10-13 08:26:25 +0800

    Any update to a bug the updated on field will be updated to the current utc date.

---
+++ BackendClassLib/Migrations/20251001055220_AddBugUpdateOnFieldAutomaticTimestamp.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendClassLib.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBugUpdateOnFieldAutomaticTimestamp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+            @"
+                CREATE TRIGGER [dbo].[Bugs_UPDATE] ON [dbo].[Bugs]
+                    AFTER UPDATE
+            AS
+            BEGIN
+                SET NOCOUNT ON;
+
+                IF ((SELECT TRIGGER_NESTLEVEL()) > 1) RETURN;
+
+                UPDATE B
+                SET UpdatedOn = GETUTCDATE()
+                FROM dbo.Bugs AS B
+                INNER JOIN INSERTED AS I
+                    ON B.Id = I.Id
+            END
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
