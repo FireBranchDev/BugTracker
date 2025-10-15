7df7572fd8309e2827f023a827d0143c96b78e90
Joshua Ryan-Sprowson <dev.joshuars@gmail.com>
2025-10-15 12:33:20 +0800

    Automatically updates updated on datetime.

---
+++ BackendClassLib/Migrations/20251015021941_AddProjectUpdatedOnAutoTimestamp.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendClassLib.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectUpdatedOnAutoTimestamp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+            @"
+                CREATE TRIGGER [dbo].[Projects_UPDATE] ON [dbo].[Projects]
+                    AFTER UPDATE
+            AS
+            BEGIN
+                SET NOCOUNT ON;
+
+                IF ((SELECT TRIGGER_NESTLEVEL()) > 1) RETURN;
+
+                UPDATE P
+                SET UpdatedOn = GETUTCDATE()
+                FROM dbo.Projects AS P
+                INNER JOIN INSERTED AS I
+                    ON P.Id = I.Id
+            END
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+            @"
+                DROP TRIGGER IF EXISTS [dbo].[Projects_UPDATE]
+            ");
+        }
+    }
+}
